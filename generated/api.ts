03ab787a7a395ead65e44970c1838df797f5db5c
honda-pp <honda-pp@users.noreply.github.com>
2024-01-28 12:27:05 +0000

    generate code from openapi yaml

--- generated/api.ts
+++ generated/api.ts
@@ -494,6 +494,42 @@ export class MemoHandler extends BaseAPI {
 export const UsersHandlerAxiosParamCreator = function (configuration?: Configuration) {
     return {
         /**
+         * Creates a new user.
+         * @summary Create a new user
+         * @param {User} user User object that needs to be added
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        createUser: async (user: User, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'user' is not null or undefined
+            assertParamExists('createUser', 'user', user)
+            const localVarPath = `/user`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'POST', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+
+    
+            localVarHeaderParameter['Content-Type'] = 'application/json';
+
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+            localVarRequestOptions.data = serializeDataIfNeeded(user, localVarRequestOptions, configuration)
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+        /**
          * Delete a single user.
          * @summary Delete user by ID
          * @param {number} id ID of user to delete
@@ -602,6 +638,19 @@ export const UsersHandlerFp = function(configuration?: Configuration) {
     const localVarAxiosParamCreator = UsersHandlerAxiosParamCreator(configuration)
     return {
         /**
+         * Creates a new user.
+         * @summary Create a new user
+         * @param {User} user User object that needs to be added
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async createUser(user: User, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<User>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.createUser(user, options);
+            const index = configuration?.serverIndex ?? 0;
+            const operationBasePath = operationServerMap['UsersHandler.createUser']?.[index]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, operationBasePath || basePath);
+        },
+        /**
          * Delete a single user.
          * @summary Delete user by ID
          * @param {number} id ID of user to delete
@@ -650,6 +699,16 @@ export const UsersHandlerFactory = function (configuration?: Configuration, base
     const localVarFp = UsersHandlerFp(configuration)
     return {
         /**
+         * Creates a new user.
+         * @summary Create a new user
+         * @param {User} user User object that needs to be added
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        createUser(user: User, options?: any): AxiosPromise<User> {
+            return localVarFp.createUser(user, options).then((request) => request(axios, basePath));
+        },
+        /**
          * Delete a single user.
          * @summary Delete user by ID
          * @param {number} id ID of user to delete
@@ -688,6 +747,18 @@ export const UsersHandlerFactory = function (configuration?: Configuration, base
  * @extends {BaseAPI}
  */
 export class UsersHandler extends BaseAPI {
+    /**
+     * Creates a new user.
+     * @summary Create a new user
+     * @param {User} user User object that needs to be added
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof UsersHandler
+     */
+    public createUser(user: User, options?: RawAxiosRequestConfig) {
+        return UsersHandlerFp(this.configuration).createUser(user, options).then((request) => request(this.axios, this.basePath));
+    }
+
     /**
      * Delete a single user.
      * @summary Delete user by ID
